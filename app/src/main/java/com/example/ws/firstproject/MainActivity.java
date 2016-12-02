d6ef6b62e2bd6117f0cb9d0b8ef9d5f9988a7375
wsparse <1500784172@qq.com>
2016-12-02 16:03:40 +0800

    this third commit

--- app/src/main/java/com/example/ws/firstproject/MainActivity.java
+++ app/src/main/java/com/example/ws/firstproject/MainActivity.java
@@ -10,4 +10,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public int  add(int a ,int b){
+        return  a+b;
+    }
 }
